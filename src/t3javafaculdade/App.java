1230191f653b57838134ff64a4a021295077c45f
Diego <diegorbrunoro@gmail.com>
2020-05-04 09:04:22 -0300

    Atualização do algoritmo criado no dia 03/05/2020

---
+++ src/t3javafaculdade/App.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t3javafaculdade;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author DIEGO
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String valor;
+        int value =0, par=0;
+        
+        ListaDupla LD = new ListaDupla();
+        ListaDupla LDPar = new ListaDupla();
+        
+        
+        Nodo nodoAtual = LD.prim;
+        
+        while(value != 99)
+        {
+        valor = JOptionPane.showInputDialog("Digite o numero: \n" + "Ou \nDigite X para Sair");
+        value = Integer.parseInt(valor);
+            if(value == 99){
+                LD.mostraLista();
+                LDPar.mostraLista();
+ 
+                 
+            }
+            if(value >=0){
+                LD.insereInicio(value); 
+            }else{
+                LD.insereFim(value);
+            }
+            nodoAtual = LD.prim;
+               
+       //-------------------------- Até aqui funciona
+ 
+                   
+        }//Fim 1º While
+        
+            while(nodoAtual != null)
+        {
+            if(nodoAtual.dado %2 == 0){
+                        LDPar.insereInicio(value);
+                    }
+            nodoAtual = nodoAtual.next;
+        }//Fim 2º While
+                    
+        }
+        
+        
+        }
